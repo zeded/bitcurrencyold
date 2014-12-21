5aa3dfa1d348b9e8ab5143dc5e7532b8552056cc
zeded <zeded@users.noreply.github.com>
2014-12-21 04:02:07 +0300

    Update sendcoinsdialog.cpp

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -37,7 +37,7 @@ SendCoinsDialog::SendCoinsDialog(QWidget *parent) :
 
 #if QT_VERSION >= 0x040700
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
-    ui->lineEditCoinControlChange->setPlaceholderText(tr("Enter a BitCurrency address (e.g. BitCurrencyfwYhBmGXcFP2Po1NpRUEiK8km2)"));
+    ui->lineEditCoinControlChange->setPlaceholderText(tr("Enter a BitCurrency address (e.g. Wbitcurrencyexampleaddress11111118km2)"));
 #endif
 
     addEntry();
